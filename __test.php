47f94b275db7fc0857929b7aa682b1a9904c5117
beuntje <benedikt@beuntje.com>
2014-08-28 10:36:32 +0200

    delete temp files

--- __test.php
+++
@@ -1,5 +0,0 @@
-<?
-	echo $_SERVER['HTTP_USER_AGENT']; 
-	echo "\n<br>\n"; 
-	echo $_SERVER['HTTP_REFERER']; 
-?>
--- _bbeuntest.php
+++
@@ -1,6 +0,0 @@
-<?php
-   
-			$strHTML = preg_replace('/\[profileimg\:([0-9]*x[0-9]*)\]/e', 'xxx', $strHTML); 
-	 
-	
-?>
