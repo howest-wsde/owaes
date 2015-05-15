f7d80e184df4058a4ae89b8147fb9a2966590e4c
Geert Hofman <geert.hofman@howest.be>
2015-05-15 11:19:54 +0200

    Kleine wijziging in tekst over gebruiksvoorwaarden

--- modal.voorwaarden.php
+++ modal.voorwaarden.php
@@ -6,7 +6,7 @@
 	$strMelding = ""; 
 	
 	$strModal = template("modal.voorwaarden.html");
-	if (!$oMe->algemenevoorwaarden()) $strMelding = "<p>U kan pas deelnemen aan het platform vanaf een OWAES-medewerker een ondertekend exemplaar van de gebruikersvoorwaarden ontvangen heeft. </p>"; 
+	if (!$oMe->algemenevoorwaarden()) $strMelding = "<p>U kan pas deelnemen aan het platform van zodra een OWAES-medewerker een ondertekend exemplaar van de gebruikersvoorwaarden ontvangen heeft. </p>"; 
 	
 	$strModal->tag("melding", $strMelding); 
 	
