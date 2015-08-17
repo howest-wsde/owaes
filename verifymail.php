7a5365a7fa8f757a916a0facbb39e541f710923e
beuntje <benedikt@beuntje.com>
2015-08-17 16:57:46 +0200

    20150817

--- verifymail.php
+++ verifymail.php
@@ -9,35 +9,37 @@
 	$oUser = user($iUser); 
 	$oUser->validateEmail($strKey); 
 	
-	if ($oUser->dienstverlener()->id() > 0) { // dienstverlener geselecteerd
-	// ER 	echo "dwel diensteverlernerne"; 
-		$oDienstverlener = $oUser->dienstverlener()->admin();  
-		$oAction = new action($oDienstverlener->id()); 
-		$oAction->type("validateuser");  
-		$oAction->data("user", $iUser); 
-		$oAction->tododate(owaestime()); 
-		$oAction->update();  
-
-		$oMail = new email(); 
-			$oDienstverlener->unlocked(TRUE); 
-			$oMail->setTo($oDienstverlener->email(), $oDienstverlener->getName());
-			$oDienstverlener->unlocked(FALSE); 
-			$oMail->template("mailtemplate.html");  
-			$strMailBody = $oUser->HTML("mail.clientingeschreven.html"); 
-			$strMailBody = str_replace("[dienstverlener]", $oUser->dienstverlener()->naam(), $strMailBody); 
-			$oMail->setBody($strMailBody);   
-			$oMail->setSubject("nieuwe OWAES inschrijving via " . $oUser->dienstverlener()->naam()); 
-		$oMail->send(); 
-	} else {
-		$arAdmins = new userlist(); 
-		$arAdmins->filter("admin"); 
-		foreach ($arAdmins->getList() as $oAdmin) {
-			$oAction = new action($oAdmin->id()); 
+	if (!$oUser->algemenevoorwaarden()) {
+		if ($oUser->dienstverlener()->id() > 0) { // dienstverlener geselecteerd
+		// ER 	echo "dwel diensteverlernerne"; 
+			$oDienstverlener = $oUser->dienstverlener()->admin();  
+			$oAction = new action($oDienstverlener->id()); 
 			$oAction->type("validateuser");  
 			$oAction->data("user", $iUser); 
 			$oAction->tododate(owaestime()); 
 			$oAction->update();  
-		} 
+	
+			$oMail = new email(); 
+				$oDienstverlener->unlocked(TRUE); 
+				$oMail->setTo($oDienstverlener->email(), $oDienstverlener->getName());
+				$oDienstverlener->unlocked(FALSE); 
+				$oMail->template("mailtemplate.html");  
+				$strMailBody = $oUser->HTML("mail.clientingeschreven.html"); 
+				$strMailBody = str_replace("[dienstverlener]", $oUser->dienstverlener()->naam(), $strMailBody); 
+				$oMail->setBody($strMailBody);   
+				$oMail->setSubject("nieuwe OWAES inschrijving via " . $oUser->dienstverlener()->naam()); 
+			$oMail->send(); 
+		} else {
+			$arAdmins = new userlist(); 
+			$arAdmins->filter("admin"); 
+			foreach ($arAdmins->getList() as $oAdmin) {
+				$oAction = new action($oAdmin->id()); 
+				$oAction->type("validateuser");  
+				$oAction->data("user", $iUser); 
+				$oAction->tododate(owaestime()); 
+				$oAction->update();  
+			} 
+		}
 	}
 	
 	redirect("index.php"); 
