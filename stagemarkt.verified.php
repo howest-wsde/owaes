d1c360a2623077d5f451daef29de4f125d8d8413
Benedikt Beun <benedikt@beuntje.com>
2016-08-26 13:23:26 +0200

    stageding: contactgegevens Rita ipv Dany

--- stagemarkt.verified.php
+++ stagemarkt.verified.php
@@ -1,14 +1,14 @@
 <?php
-	include "inc.default.php"; // should be included in EVERY file 
-	  
-	$oSecurity = new security(FALSE); 
-	 
+	include "inc.default.php"; // should be included in EVERY file
+
+	$oSecurity = new security(FALSE);
+
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <?php
-        	echo $oPage->getHeader(); 
-		?> 
+        	echo $oPage->getHeader();
+		?>
     </head>
     <body id="login">
     <nav class="navbar navbar-default">
@@ -26,19 +26,19 @@
             </div>
         </div>
     </nav>
-    
+
     	<div class="body container content content-login">
-            <div class="row"> 
+            <div class="row">
                 <div class="signup col-lg-12">
-                    <div class="well"> 
+                    <div class="well">
                         <p>Bedankt, uw e-mailadres werd gevalideerd en uw inschrijving correct ontvangen. </p>
-                        <p>Wenst u meer informatie, contacteer dan Dany Cottyn via <a href="mailto:dany.cottyn@howest.be">dany.cottyn@howest.be</a> of op 0484 85 13 36.</p>
+                        <p>Wenst u meer informatie, contacteer dan Rita Vanmeirhaeghe  via <a href="mailto:stage@howest.be">dany.cottyn@howest.be</a> of op 056/24 12 44.</p>
                     </div>
                 </div>
-            </div> 
+            </div>
         </div>
-		 
-        <div class="footer"> 
+
+        <div class="footer">
         </div>
     </body>
 </html>
